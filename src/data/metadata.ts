0051ab1fd3bdeb0c506176c32cf0794a3487bce9
Tatsuya <a.tatsu0117@gmail.com>
2025-07-28 18:03:48 +0900

    seoと404page

---
+++ src/data/metadata.ts
@@ -0,0 +1,23 @@
+export const SITE_NAME = "ECHive";
+export const OG_IMAGE_URL = "https://ec.hive-corp.jp/img/ogp.png";
+
+export const TOP_METADATA = {
+  title: `${SITE_NAME} | ECサイト開設・運用代行サービス`,
+  description:
+    "ECHiveは、Shopify専門のECサイト制作代行サービスです。最短1週間でオンラインショップ開設！初期設定・デザイン・商品登録・運用サポートまでワンストップで対応いたします。",
+};
+
+export const PRIVACY_POLICY_METADATA = {
+  title: `プライバシーポリシー | ${SITE_NAME}`,
+  description: "ECHiveのプライバシーポリシーです。",
+};
+
+export const THANKS_METADATA = {
+  title: `お問い合わせ完了 | ${SITE_NAME}`,
+  description: "お問い合わせありがとうございました。",
+};
+
+export const NOT_FOUND_METADATA = {
+  title: `お探しのページは見つかりませんでした。 | ${SITE_NAME}`,
+  description: "お探しのページは見つかりませんでした。",
+};
