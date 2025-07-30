85cd0a0424c3d3f380413e26ffb20f183b0c9779
Tatsuya <a.tatsu0117@gmail.com>
2025-07-30 09:41:58 +0900

    ga added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ec.hive-corp.jp",
@@ -22,5 +24,13 @@ export default defineConfig({
     },
   },
 
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
